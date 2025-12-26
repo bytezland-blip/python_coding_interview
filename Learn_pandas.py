e1c6d25a3b1fbb2819dc8f79b64ca711a94617c1
KALIDOSS619 <skalidoss619@gmail.com>
2025-12-26 08:53:25 +0530

    new pandas changes

--- Learn_pandas.py
+++ Learn_pandas.py
@@ -2,8 +2,10 @@
 import pandas as pd
 
 df = pd.DataFrame({
-    "emp_name" :["vj","bj", "cj", "vij","bij", "cij"] 
-    , "emp_no" : [1, 2, 3, 4, 5,6]})
+    "emp_name" :["vj","bj", "cj", "vij","bij", "cij"] ,
+    "emp_no" : [1, 2, 3, 4, 5,6],
+    "age" : [32,34,45,56,33,36]}
+                  )
 df1 = pd.DataFrame({
     "emp_no": [1, 2, 3, 4, 5, 6],
     "salary": [20, 39, 34, 23 ,65, 48]
@@ -16,24 +18,14 @@ inner_join = pd.merge(df, df1, on="emp_no", how="inner")
 outer_join = pd.merge(df, df1, on="emp_no", how="outer")
 
 print(mer)
-print()
-print(right_join)
-print()
-print(left_join)
-print()
-print(inner_join)
-print()
-print(outer_join)
-print()
+
 
 print(f"mid value of the list = {mer["salary"].median()}")
-print()
+
 print(f"sum the all values = {mer["salary"].sum()}")
-print()
+
 print(f"find the mean value ={mer["salary"].mean()}")
-print()
 print(f"find the how many null values are there = {mer["salary"].count()}")  
-print()
 print(f"find the size of row {mer["salary"].size()}")
 
 #secound heightest number
